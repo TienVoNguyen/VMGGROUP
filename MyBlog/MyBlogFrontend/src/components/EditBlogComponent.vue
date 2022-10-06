488fcc0004bfdf14eb4557445b7326e05af0eab7
TienVoNguyen <vontph18623@fpt.edu.vn>
2022-10-06 08:57:12 +0700

    update

--- MyBlog/MyBlogFrontend/src/components/EditBlogComponent.vue
+++ MyBlog/MyBlogFrontend/src/components/EditBlogComponent.vue
@@ -38,7 +38,69 @@
 
 <script>
 export default {
-  name: "EditBlogComponent"
+  name: "EditBlogComponent",
+  data() {
+    return {
+      blog: {
+        title: '',
+        categoryCode: '',
+        content: '',
+        cover: []
+      },
+      categories: [],
+      rules: {
+        title: [
+          {required: true, message: 'Please input title blog', trigger: 'blur'},
+          {min: 3, max: 20, message: 'Length should be 3 to 20', trigger: 'blur'}
+        ],
+        categoryCode: [
+          {required: true, message: 'Please select Category ', trigger: 'change'}
+        ],
+        content: [
+          {required: true, message: 'Please input content bolg', trigger: 'blur'}
+        ],
+        cover: [
+          {required: true, message: 'Please input cover bolg', trigger: 'change'}
+        ]
+      }
+    };
+  },
+  methods: {
+    test() {
+      console.log(this.blog.categoryCode)
+    },
+    vali(formName) {
+      this.$refs[formName].validate((valid) => {
+        console.log(valid)
+      })
+    },
+
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          let form = document.querySelector('#form-blog');
+          console.log(form);
+          BlogDataService.create(form);
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  },
+  created() {
+    CategoryDataService.getAll()
+        .then(response => {
+          this.categories = response.data
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+  }
 }
 </script>
 
